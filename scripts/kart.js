3b297ec022efef4016f9486ee3ea35df8361abac
Sanket Patil <patilsanket209@gmail.com>
2022-01-23 01:10:52 +0530

    Cart done

--- scripts/kart.js
+++ scripts/kart.js
@@ -5,9 +5,10 @@ document.getElementById("nav").innerHTML=header();
 
 
 let data=JSON.parse(localStorage.getItem("cartarray"))
-let y=document.getElementById("left")
+let y=document.getElementById("le")
 console.log(data)
 let x=``; 
+let top=0; 
 data.forEach((ele)=>{
 x+=`  <div id="box">
 <div id="im"> <img src="${ele.image}" ></div>
@@ -23,6 +24,19 @@ x+=`  <div id="box">
 </div>
 
 </div>`
- 
+ top+=ele.price; 
 })
 y.innerHTML=x; 
+  let b=Math.floor(top*0.2); 
+ let a =top- b
+let z=` <div class="first"><H3>Price Details </H3></div>
+<div><h4>Price <span class="tp1">₹ ${top}</span></h4>
+<h4>Discount <span class="tp2"> -₹${b}</span></h4>
+<h4>Delivery Charges <span class="tp">Free</span></h4></div>
+<hr>
+<div>
+<h3> Total Amount  <span class="tp">₹ ${a}</span></h3>
+</div>
+<hr>
+<h4>  You will Save ₹${b} on this order</h4>`
+  document.getElementById("right").innerHTML=z; 

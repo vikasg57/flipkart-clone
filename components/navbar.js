db2f2e9e5d4c96c13a8c16ff300658f3e9ffad7c
Sanket Patil <patilsanket209@gmail.com>
2022-01-22 09:58:17 +0530

    Final push

---
+++ components/navbar.js
@@ -0,0 +1,23 @@
+function navbar(){
+    return `
+    <div>
+      <img src="https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png" alt="">
+    </div>
+    <div id="search"> <div > <input type="text"> ⭕ </div> </div>
+   
+  
+   <div class="opt">
+     <div class="lbtn"><button>Login </button></div>
+     <div > 
+       <select>
+         <option value="">More</option>
+         <option value="">fadfad</option>
+         <option value="">adfad</option>
+         <option value="">asdfd</option>
+       </select>
+     </div>
+     <div>🛒Cart</div>
+   </div>`
+}
+
+export default navbar; 
--- script/signup.js
+++ script/signup.js
@@ -4,3 +4,8 @@
     let x= document.getElementById("mobile").value; 
     localStorage.setItem("mobile",JSON.stringify(x));
  }
+
+ import navbar  from "../components/navbar.js";
+
+ let x=navbar();
+ document.getElementById("nav").innerHTML=x; 
